1d247591e58969d22740d1ce6ce8c0855c98d07d
kurotori <kurotori4423@gmail.com>
2025-10-22 16:05:15 +0900

    不要なステップを削除

--- .github/workflows/JsScripts/GetRepositoryOptions.mjs
+++
@@ -1,9 +0,0 @@
-import { getAvailableRepositories } from './ConfigHelper.mjs';
-
-/**
- * 利用可能なリポジトリオプションをGitHub Actionsの選択肢形式で出力する
- */
-export function getRepositoryOptions() {
-    const repositories = getAvailableRepositories();
-    return repositories;
-}
